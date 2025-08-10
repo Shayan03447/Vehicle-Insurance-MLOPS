198ce86a3969529f0b97540c182f5c33ba80e63b
Shayan <shanich03447@gmail.com>
2025-08-10 15:06:58 +0500

    Project structure ready venv set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.01",
+    author="Shayan Ali",
+    author_email="shanich03447@gmail.com",
+    packages=find_packages()
+
+)
